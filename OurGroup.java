ad72e536543ae2b3db38ec55118904d05c2b8494
nutnareexx <91516503+nutnareexx@users.noreply.github.com>
2022-01-25 17:02:28 +0700

    Update OurGroup.java

--- OurGroup.java
+++ OurGroup.java
@@ -6,7 +6,7 @@ public class OurGroup {
     public OurGroup()
     {
         groupMembers = new Vector<String>();
-        groupMembers.add("YOUR NAME"); //PUT YOUR NAME HERE
+        groupMembers.add("Nutnaree Comdenlek"); //PUT YOUR NAME HERE
     }
     public Vector<String> getGroupMembers () {
         return groupMembers;
