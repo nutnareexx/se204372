9dc99bf05fabc1f7e03e9a1bea641b7b937ca83b
nutnareexx <nutnaree.co@ku.th>
2022-01-26 15:52:35 +0700

    final

--- OurGroup.java
+++ OurGroup.java
@@ -6,7 +6,7 @@ public class OurGroup {
     public OurGroup()
     {
         groupMembers = new Vector<String>();
-        groupMembers.add(""); //PUT YOUR NAME HERE
+        groupMembers.add("Nutnaree Comdenlek"); //PUT YOUR NAME HERE
         groupMembers.add("Mananchaya Waitayakul");
         groupMembers.add("Potjarin Prasertsri");
         groupMembers.add("Kanyanut Tangcharoen");
