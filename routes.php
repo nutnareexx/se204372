985d25a8718aa12fc96f4d46a15146af26a1cab5
KanyanutM <91516461+KanyanutM@users.noreply.github.com>
2022-03-05 02:23:21 +0700

    Create routes.php

---
+++ routes.php
@@ -0,0 +1,18 @@
+<?php
+$controllers = array('pages'=>['home','error']);
+
+function call($controller,$action){
+    echo "routes to ".$controller."-".$action."<br>";
+}
+
+if(array_key_exists($controller,$controllers))
+{
+    if(in_array($action,$controllers[$controller]))
+    {   call($controller,$action);  }
+    else
+    {   call('pages','home');    }
+}
+else
+{
+    call('pages','home');
+}?>
