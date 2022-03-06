c3213640fb4d0a60581765fecaa1fde1b7191fe4
Potjarinn <potjarin.p@ku.th>
2022-03-07 03:21:11 +0700

    Update petitionDc_controller.php

--- controllers/petitionDc_controller.php
+++ controllers/petitionDc_controller.php
@@ -6,5 +6,16 @@ class petitionDcController
         $petionDcList = petitionDcModel::getAll();
         require_once('views/petitionDc/index_petitionDc.php');
     }
+
+    public function newpetitionDc()
+    {
+        $petionDcList = petitionDcModel::getAll();
+        $approveList = approveModel::getAll();
+        $statusList = statusModel::getAll();
+        $nametitleList = nametitleModel::getAll();
+        $userList = userModel::getAll();
+        $DetailCompanyList = DetailCompanyModel::getAll();
+        require_once('views/petitionDc/newpetitionDc.php');
+    }
 }
 ?>
