8a441063aa55fbe499fdfdf67c345c2e6c7ff050
KanyanutM <91516461+KanyanutM@users.noreply.github.com>
2022-03-07 01:53:51 +0700

    Update index_cooperative.php

--- views/cooperative/index_cooperative.php
+++ views/cooperative/index_cooperative.php
@@ -16,28 +16,7 @@
             }
 
 
-            .btn-group .button {
-            /*border-radius: 5%;*/
-            position: relative;
-            background-color: #0974ba ; /* Green */
-            border: 1px black;
-            color: white;
-            padding: 15px 32px;
-            text-align: center;
-            text-decoration: none;
-            display: inline-block;
-            font-size: 16px;
-            cursor: pointer;
-            /*float: center;*/
-        }
-
-        .btn-group .button:not(:last-child) {
-            border-right: none; /* Prevent double borders */
-        }
-
-        .btn-group .button:hover {
-            background-color: #ddd;
-            color: black;
+        
 
 table {
     font-family: 'Prompt', sans-serif;
