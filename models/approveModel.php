78bc0ef9302e739a1f94678f77113305a351fa29
Potjarinn <potjarin.p@ku.th>
2022-03-08 03:38:48 +0700

    Update approveModel.php

--- models/approveModel.php
+++ models/approveModel.php
@@ -10,7 +10,7 @@ class approveModel
     {
         
         $this->approve_id = $approve_id;
-        $this->approve_name = $approve_date;
+        $this->approve_date = $approve_date;
         $this->status_id = $status_id;
         $this->status_name = $status_name;
 
@@ -20,12 +20,12 @@ class approveModel
     {
         $approveList = [];
         require("connection_connect.php");
-        $sql = "SELECT * FROM approve as a NATURAL JOIN status as s WHERE a.status_id=s.status_id;";
+        $sql = "SELECT * FROM `approve` as a NATURAL JOIN status as s WHERE a.status_id=s.status_id";
         $result = $conn->query($sql);
         while($my_row = $result->fetch_assoc())
         {
             $approve_id = $my_row['approve_id'];
-            $approve_name = $my_row['approve_name'];
+            $approve_date = $my_row['approve_date'];
             $status_id = $my_row['status_id'];
             $status_name = $my_row['status_name'];
             $approveList[] = new approveModel($approve_id,$approve_date,$status_id,$status_name);
@@ -39,11 +39,11 @@ class approveModel
     public static function get($approve_id)
     {
         require("connection_connect.php");
-        $sql = "SELECT * FROM approve as a NATURAL JOIN status as s WHERE a.status_id=s.status_id;";
+        $sql = "SELECT * FROM `approve` as a NATURAL JOIN status as s WHERE a.status_id=s.status_id";
         $result = $conn->query($sql);
-        $my_row = $result->fetch_assoc()
+        $my_row = $result->fetch_assoc();
         $approve_id = $my_row['approve_id'];
-        $approve_name = $my_row['approve_name'];
+        $approve_date = $my_row['approve_date'];
         $status_id = $my_row['status_id'];
         $status_name = $my_row['status_name'];
         require("connection_close.php");
