db69e820fe2b285d3b19571c3a631baed7c29515
KanyanutM <91516461+KanyanutM@users.noreply.github.com>
2022-03-10 01:40:45 +0700

    Create index_hamburgerAj.php

---
+++ views/hamburger/index_hamburgerAj.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+<meta charset="UTF-8">
+        <title> Drop Down Sidebar Menu | CodingLab </title>
+        <link rel ="stylesheet" href="style.css">
+        <link href='https://unpkg.com/boxicons@2.1.1/css/boxicons.min.css' rel='stylesheet'>
+        <link href="https://fonts.googleapis.com/css2?family=Prompt&display=swap" rel="stylesheet">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+<style>
+body {
+    font-family: 'Prompt', sans-serif;
+}
+
+
+
+
+
+</style>
+</head>
+<body>
+
+<div class="sidebar ">
+        <div class="logo-details">
+        <i class='bx bxs-tree-alt'></i>
+        <span class="logo_name">KASETSART UNIVERSITY</span>
+    </div>
+    <a class="nav-links">
+
+            <a href="#">
+                <i class='bx bx-file-blank' ></i>
+                <span class="link_name">สถานประกอบการณ์</span>
+            
+        
+
+
+
+</body>
+</html> 
