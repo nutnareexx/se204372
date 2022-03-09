4fb9a209e6bbe6aa2931ed0bb2400bf12822d85d
KanyanutM <91516461+KanyanutM@users.noreply.github.com>
2022-03-10 01:40:51 +0700

    Update index_hamburger.php

--- views/hamburger/index_hamburger.php
+++ views/hamburger/index_hamburger.php
@@ -18,19 +18,20 @@ body {
     position: fixed;
     top: 0;
     left: 0;
-    
     height: 100%;
     width: 360px;
-    background: #11101d;
-   
-    padding-top: 20px;
+    background: #397d54;
+    z-index: 100;
+    transition: all 0.5s ease;
 }
 
+.sidebar.close{
+    width: 78px;
+}
 
-
-.sidebar a:hover {
+/*.sidebar a:hover {
     color: #f1f1f1;
-}
+}*/
 .sidebar .logo-details{
     height: 60px;
     width: 100%;
@@ -39,27 +40,38 @@ body {
 }
 
 .sidebar .logo-details i{
-    font-size: 50px;
+    font-size: 30px;
     color: #fff;
     height: 50px;
-    min-width: 120px;
-    text-align: right;
+    min-width: 78px;
+    text-align: center;
     line-height:50px;
+    
 }
 
 .sidebar .logo-details .logo_name{
-    
     font-size: 22px;
     color: #fff;
     font-weight: 600;
-    text-align: center;
+    transition:  0.3s ease;
+    transition-delay: 0.1s;
+}
+
+.sidebar.close .logo-details .logo_name{
+    transition-delay: 0s;
+    opacity: 0;
+    pointer-events: none;
 }
 
 .sidebar .nav-links{
-        /*background: red;*/
+     /*   background: red;*/
     height: 100%;
     padding-top:30px 0 150px 0;
     overflow: auto;
+}
+
+.sidebar .nav-links{
+    overflow: visible:
 }
 
 .sidebar .nav-links::-webkit-scrollbar{
@@ -73,7 +85,7 @@ body {
 }
 
 .sidebar .nav-links li:hover{
-    background: #1d1b31;
+    background: #73c088;
 }
 
 .sidebar .nav-links li .iocn-link{
@@ -82,37 +94,55 @@ body {
     justify-content: space-between;
 }
 
+.sidebar.close .nav-links li .iocn-link{
+    display: block;
+}
+
 .sidebar .nav-links li i{
     height: 50px;
-    min-width: 50px;
+    min-width: 78px;
     text-align: center;
     line-height: 50px;
     color: #fff;
     font-size: 30px;
+    cursor: pointer;
     transition: all 0.3s ease;
+    
+    
 }
 
 .sidebar .nav-links li.showMenu i.arrow{
     transform: rotate(-180deg);
+}
+.sidebar.close .nav-links i.arrow{
+    display: none;
 }
 
 .sidebar .nav-links li a{
     display: flex;
     align-items: center;
     text-decoration: none;
+    white-space: nowrap;
 }
 
 .sidebar .nav-links li a .link_name{
     font-size: 18px;
     font-weight: 400;
     color: #fff;
+  
+    
+}
+.sidebar.close .nav-links li a .link_name{
+    opacity: 0;
+    pointer-events: none;
 }
 
 .sidebar .nav-links li .sub-menu{
     padding: 6px 6px 14px 80px;
     margin-top: -10px;
-    background: #1d1b31;
+    background: #397d54;
     display: none;
+    
 }
 
 .sidebar .nav-links li.showMenu .sub-menu{
@@ -139,9 +169,11 @@ body {
     margin-top: 0;
     padding: 10px 20px;
     border-radius: 0 6px 6px 0;
-    transition: all 0.4s ease;
+    
     opacity: 0;
+    display: block;
     pointer-events: none;
+    transition: 0s;
 }
 
 
@@ -149,34 +181,40 @@ body {
     top: 0;
     opacity: 1;
     pointer-events: auto;
+    transition: all 0.4s ease;
 }
 
 .sidebar .nav-links li .sub-menu .link_name{
     display: none;
 }
+
+
 .sidebar.close .nav-links li .sub-menu .link_name{
     font-size: 18px;
     opacity: 1;
     display: block;
 }
 
-.sidebar .nav-link li .sub-menu .blank{
+.sidebar .nav-link li .sub-menu.blank{
     opacity: 1;
     pointer-events: auto;
     padding: 3px 20px 6px 16px;
+    opacity: 0;
+    pointer-events: none;
+   
 }
 
-.sidebar .nav-link li:hover .sub-menu .blank{
-    top: 5;
+.sidebar .nav-link li:hover .sub-menu.blank{
+    top: 50%;
     transform: translateY(-50%);
 }
 
 .home-section{
     position: relative;
-    background: #cae3cc;
+    background: #fff;
     height: 100vh;
-    left: 352px;
-    width: calc(100% - 250px);
+    left: 360px;
+    width: calc(100% - 360px);
     transition: all 0.5s ease;
 }
 
@@ -185,25 +223,58 @@ body {
     width: calc(100% - 78px);
 }
 
+.home-content{
+    background: #235d3a;
+}
+
 .home-section .home-content{
     height: 60px;
     display: flex;
     align-items: center;
+    
 }
 
 .home-section .home-content .bx-menu,
 .home-section .home-content .text{
-    color: #11101d;
+    color: #fff;
     font-size: 35px;
+    white-space: nowrap;
 }
 
 .home-section .home-content .bx-menu{
     margin: 0 15px;
+    cursor: pointer;
 }
 
 .home-section .home-content .text{
     font-size: 26px;
-    font-weight:600;
+    font-weight: 500;
+   
+}
+
+.sidebar .profile-details{
+    position: fixed;
+    bottom: 0;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+}
+
+.sidebar .profile-details i{
+    height: 52px;
+    width: 100%;
+    align-items: center;
+    color: #fff;
+    line-height: 50px;
+    font-size: 40px;
+    margin: 0 20px 0 20px;
+}
+
+.sidebar .profile-details .profile_name{
+    color: #fff;
+    font-size: 20px;
+    font-weight: 500;
+    margin: 0 70px 0 70px;
 }
 
 
@@ -217,10 +288,10 @@ body {
 
 <div class="sidebar ">
         <div class="logo-details">
-        <i class='bx bxs-tree-alt'></i>
-        <span class="logo_name">KASETSART</span>
-    </div>
-    <ul class="nav-links">
+            <i class='bx bxs-tree-alt'></i>
+            <span class="logo_name">KASETSART UNIVERSITY</span>
+        </div>
+    <dl class="nav-links">
         <li>
             <a href="#">
                 <i class='bx bx-file-blank' ></i>
@@ -261,7 +332,18 @@ body {
             <ul class="sub-menu blank">
                 <li><a class="link_name" href="#">upload</a></li>
             </ul>
-    </ul>
+        </li>
+    </dl>
+    <div class="profile-details">
+        <div class="profile-content">
+            <i class='bx bxs-user'></i>
+        </div>
+        <div class="name-job">
+            <div class="profile_name">Name</div>
+        </div>
+        <i class='bx bx-log-out'></i>
+    </div>
+    </li>
 </div>
 
 <section class="home-section">
@@ -277,7 +359,6 @@ let arrow = document.querySelectorAll(".arrow");
 for (var i = 0; i < arrow.length; i++){
     arrow[i].addEventListener("click", (e)=>{
         let arrowParent = e.target.parentElement.parentElement;
-        
         arrowParent.classList.toggle("showMenu");
     });
 }
