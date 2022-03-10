0fcc8094f454e7dfc7256dfe17812458c8f0e4f7
KanyanutM <91516461+KanyanutM@users.noreply.github.com>
2022-03-11 01:07:37 +0700

    Create index_graphAj.php

---
+++ views/graphAj/index_graphAj.php
@@ -0,0 +1,150 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+        <link rel ="stylesheet" href="hamburger.css">
+        <link href='https://unpkg.com/boxicons@2.1.1/css/boxicons.min.css' rel='stylesheet'>
+        <link href="https://fonts.googleapis.com/css2?family=Prompt&display=swap" rel="stylesheet">
+        
+    
+
+</head>
+<body>
+
+<div class="sidebar ">
+        <div class="logo-details">
+            <i class='bx bxs-tree-alt'></i>
+            <span class="logo_name">KASETSART UNIVERSITY</span>
+        </div>
+    <dl class="nav-links">
+        <li>
+            <div class="iocn-link">
+                <a href="#">
+                    <i class='bx bx-buildings'></i>
+                    <span class="link_name">สถานประกอบการณ์</span>
+                </a>
+                <i class='bx bx-chevron-down arrow'></i>
+            </div>
+            <ul class="sub-menu">
+                <li><a class="link_name" href="#">สถานประกอบการณ์</a></li>
+                <li><a href="?controller=companyP&action=indexP">ฝึกงาน</a></li>
+                <li><a href="?controller=companyC&action=index">สหกิจ</a></li>
+            </ul>
+        </li>
+        <li>
+            <div class="iocn-link">
+                <a href="#">
+                    <i class='bx bx-check-square'></i>
+                    <span class="link_name">ตรวจสอบคำร้อง</span>
+                </a>
+                <i class='bx bx-chevron-down arrow'></i>
+            </div>
+            <ul class="sub-menu">
+                <li><a class="link_name" href="#">ตรวจสอบคำร้อง</a></li>
+                <li><a href="?controller=petitionDC&action=index">ฝึกงาน</a></li>
+                <li><a href="?controller=petitionC&action=indexC">สหกิจ</a></li>
+            </ul>
+        </li>
+        
+        <li>
+            <a href="?controller=considerAj&action=index">
+                <i class='bx bx-file'></i>
+                <span class="link_name">พิจาราณาคำร้องขอฝึกงาน</span>
+            </a>
+            <ul class="sub-menu blank">
+                <li><a class="link_name" href="#">พิจาราณาคำร้องขอฝึกงาน</a></li>
+            </ul>
+        </li>
+        <li>
+            <a href="?controller=graphAj&action=index">
+                <i class='bx bx-line-chart' ></i>
+                <span class="link_name">สถานการณ์ยื่นคำร้องนิสิต</span>
+            </a>
+            <ul class="sub-menu blank">
+                <li><a class="link_name" href="#">สถานการณ์ยื่นคำร้องนิสิต</a></li>
+            </ul>
+        </li>
+        <li>
+            <a href="?controller=reportPetition&action=index">
+                <i class='bx bx-file-blank' ></i>
+                <span class="link_name">รายงานสรุปการอนุมัติฝึกงาน</span>
+            </a>
+            <ul class="sub-menu blank">
+                <li><a class="link_name" href="#">รายงานสรุปการอนุมัติฝึกงาน</a></li>
+            </ul>
+        </li>
+        <li>
+            <a href="?controller=bookAj&action=index">
+                <i class='bx bx-book' ></i>
+                <span class="link_name">ออกหนังสือขอความอนุเคราะห์</span>
+            </a>
+            <ul class="sub-menu blank">
+                <li><a class="link_name" href="#">ออกหนังสือขอความอนุเคราะห์</a></li>
+            </ul>
+        </li>
+        <li>
+            <a href="?controller=checkHistory&action=index">
+                <i class='bx bx-notepad'></i>
+                <span class="link_name">ตรวจสอบประวัติการฝึกงาน</span>
+            </a>
+            <ul class="sub-menu blank">
+                <li><a class="link_name" href="#">ตรวจสอบประวัติการฝึกงาน</a></li>
+            </ul>
+        </li>
+        <li>
+            <a href="?controller=uploadAj&action=index">
+                <i class='bx bx-upload'></i>
+                <span class="link_name">อัพโหลดประกาศ</span>
+            </a>
+            <ul class="sub-menu blank">
+                <li><a class="link_name" href="#">อัพโหลดประกาศ</a></li>
+            </ul>
+        </li>
+        <li>
+        <div class="profile-details">
+            <div class="profile-content">
+                <i class='bx bxs-user'></i>
+            </div>
+            
+                <div class="name-job">
+                    <div class="profile_name">Name</div>
+                </div>
+            <i class='bx bx-log-out '></i>
+        </div>
+        </li>
+    </dl>
+</div>
+
+<section class="home-section">
+    <div class="home-content">
+        <i class='bx bx-menu'></i>
+        <span class="text">ระบบบริหารจัดการการฝึกงานแบบออนไลน์</span>
+    </div>
+    <!-- เขียนตรงนี้นาจาาาาา -->
+    <div class="header">
+        <h2>สถานการณ์ยื่นคำร้องนิสิต</h2>
+    </div>
+</section>
+
+<script>
+
+let arrow = document.querySelectorAll(".arrow");
+for (var i = 0; i < arrow.length; i++){
+    arrow[i].addEventListener("click", (e)=>{
+        let arrowParent = e.target.parentElement.parentElement;
+        arrowParent.classList.toggle("showMenu");
+    });
+}
+
+let sidebar = document.querySelector(".sidebar");
+let sidebarBtn = document.querySelector(".bx-menu");
+console.log(sidebarBtn);
+sidebarBtn.addEventListener("click", ()=>{
+    sidebar.classList.toggle("close");
+});
+
+</script>
+
+
+</body>
+</html> 
