83de1f6dcfa9ba4b996e79f303fd8bce63c348a8
Potjarinn <potjarin.p@ku.th>
2022-03-10 21:05:41 +0700

    Create newdetailCompany.php

---
+++ views/detailCompany/newdetailCompany.php
@@ -0,0 +1,17 @@
+<form method="get" action="">
+<label> ชื่อสถานประกอบการ/หน่วยงาน <input type = "text" name = "dc_name" /> </label></br>
+<label> ตำแหน่งงานที่ต้องการรับ <input type = "text" name = "dc_position" /> </label></br>
+<label> แผนกฝ่าย <input type = "text" name = "dc_department" /> </label></br>
+<label> จำนวน(คน) <input type = "text" name = "dc_num" /> </label></br>
+<label> ความสามารถทีมีหรือทักษะที่ควรมี <input type = "text" name = "dc_skills" /> </label></br>
+<label> ลักษณะงานที่ต้องปฏิบัติ <input type = "text" name = "dc_nature" /> </label></br>
+<label> จำนวนค่าตอบแทน <input type = "text" name = "dc_pay" /> </label></br>
+<label> ที่พัก <input type = "text" name = "dc_room" /> </label></br>
+<label> สวัสดิการอื่นๆ <input type = "text" name = "dc_benefit" /> </label></br>
+<label> การคัดเลือก <input type = "text" name = "dc_select" /> </label></br>
+
+<input type="hidden" name="controller" value="company"/>
+<button type="submit" name="action" value="index">Back</button>
+<button type="submit" name="action" value="addDetailCompany">Save</button>
+
+</form>
