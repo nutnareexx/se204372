d54ddbc130c9ff9b4e98a1bebc8a7e5be670ea02
Potjarinn <potjarin.p@ku.th>
2022-03-10 21:05:56 +0700

    Update company_controller.php

--- controllers/company_controller.php
+++ controllers/company_controller.php
@@ -6,5 +6,37 @@ class companyController
         $DetailCompanyList = DetailCompanyModel::getAll();
         require_once('views/detailCompany/index_detailCompany.php');
     }
+
+    public function newDetailCompany()
+    {
+        $DetailCompanyList = DetailCompanyModel::getAll();
+        require_once('views/detailCompany/newdetailCompany.php');
+    }
+
+    public function addDetailCompany()
+    {
+        $dc_name = $_GET['dc_name'];
+        $dc_position = $_GET['dc_position'];
+        $dc_department = $_GET['dc_department'];
+        $dc_num = $_GET['dc_num'];
+        $dc_skills = $_GET['dc_skills'];
+        $dc_nature = $_GET['dc_nature'];
+        $dc_pay = $_GET['dc_pay'];
+        $dc_room = $_GET['dc_room'];
+        $dc_benefit = $_GET['dc_benefit'];
+        $dc_select = $_GET['dc_select'];
+        DetailCompanyModel::add($dc_name,$dc_position,$dc_department,$dc_num,$dc_skills,$dc_nature,$dc_pay,$dc_room,$dc_benefit,$dc_select);
+        companyController::index();
+    }
+
+    public function search()
+    {
+        $key = $_GET['key'];
+        
+        $DetailCompanyList = DetailCompanyModel::search($key);
+        require_once('views/detailCompany/index_detailCompany.php');
+    }
+
+
 }
 ?>
