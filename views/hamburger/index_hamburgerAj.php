ee181287e32c71d934bea364a4a3e08c1eb15e09
KanyanutM <91516461+KanyanutM@users.noreply.github.com>
2022-03-11 01:07:39 +0700

    Update index_hamburgerAj.php

--- views/hamburger/index_hamburgerAj.php
+++ views/hamburger/index_hamburgerAj.php
@@ -49,7 +49,7 @@
         </li>
         
         <li>
-            <a href="#">
+            <a href="?controller=considerAj&action=index">
                 <i class='bx bx-file'></i>
                 <span class="link_name">พิจาราณาคำร้องขอฝึกงาน</span>
             </a>
@@ -58,7 +58,7 @@
             </ul>
         </li>
         <li>
-            <a href="#">
+            <a href="?controller=graphAj&action=index">
                 <i class='bx bx-line-chart' ></i>
                 <span class="link_name">สถานการณ์ยื่นคำร้องนิสิต</span>
             </a>
@@ -67,7 +67,7 @@
             </ul>
         </li>
         <li>
-            <a href="#">
+            <a href="?controller=reportPetition&action=index">
                 <i class='bx bx-file-blank' ></i>
                 <span class="link_name">รายงานสรุปการอนุมัติฝึกงาน</span>
             </a>
@@ -76,7 +76,7 @@
             </ul>
         </li>
         <li>
-            <a href="#">
+            <a href="?controller=bookAj&action=index">
                 <i class='bx bx-book' ></i>
                 <span class="link_name">ออกหนังสือขอความอนุเคราะห์</span>
             </a>
@@ -85,7 +85,7 @@
             </ul>
         </li>
         <li>
-            <a href="#">
+            <a href="?controller=checkHistory&action=index">
                 <i class='bx bx-notepad'></i>
                 <span class="link_name">ตรวจสอบประวัติการฝึกงาน</span>
             </a>
@@ -94,7 +94,7 @@
             </ul>
         </li>
         <li>
-            <a href="#">
+            <a href="?controller=uploadAj&action=index">
                 <i class='bx bx-upload'></i>
                 <span class="link_name">อัพโหลดประกาศ</span>
             </a>
