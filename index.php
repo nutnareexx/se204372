f4540f0f88eef241bbc95c55a79ec7b89d93443c
KanyanutM <91516461+KanyanutM@users.noreply.github.com>
2022-03-11 01:07:21 +0700

    Update index.php

--- index.php
+++ index.php
@@ -20,7 +20,7 @@ else
         <?php echo "controller = ".$controller." ,action = ".$action ;?>
         <br>
         [<a href= "?controller=pages&action=home">Home</a>]
-        [<a href= "?controller=petitionDCUser&action=index">ยื่นคำร้องฝึกงานสำหรับนิสิต</a>]
+        <!--[<a href= "?controller=petitionDCUser&action=index">ยื่นคำร้องฝึกงานสำหรับนิสิต</a>]
         [<a href= "?controller=petitionCUser&action=index">ยื่นคำร้องสหกิจสำหรับนิสิต</a>]
         [<a href= "?controller=checkApproval&action=index">ตรวจสอบผลการอนุมัตินิสิต</a>]
         [<a href= "?controller=downloadStudent&action=index">downloadนิสิต</a>]
@@ -30,15 +30,15 @@ else
         [<a href= "?controller=company&action=index">companyฝึกงาน</a>]
         [<a href= "?controller=cooperative&action=index">companyสหกิจ</a>]
         [<a href= "?controller=companyP&action=indexP">companyฝึกงานสำหรับอาจารย์</a>]
-        [<a href= "?controller=companyC&action=index">companyสหกิจสำหรับอาจารย์</a>]
+        [<a href= "?controller=companyC&action=index">companyสหกิจสำหรับอาจารย์</a>]-->
         [<a href= "?controller=hamburger&action=index">hamburgerนิสิต</a>]
         [<a href= "?controller=hamburgerAj&action=index">hamburgerอาจารย์</a>]
-        [<a href= "?controller=considerAj&action=index">พิจาราณาคำร้อง/อาจารย์</a>]
+        <!--[<a href= "?controller=considerAj&action=index">พิจาราณาคำร้อง/อาจารย์</a>]
         [<a href= "?controller=graphAj&action=index">กราฟคำร้อง/อาจารย์</a>]
         [<a href= "?controller=reportPetition&action=index">รายงานสรุป/อาจารย์</a>]
         [<a href= "?controller=bookAj&action=index">ออกหนังสือ/อาจารย์</a>]
         [<a href= "?controller=checkHistory&action=index">ตรวจสอบประวัติ/อาจารย์</a>]
-        [<a href= "?controller=uploadAj&action=index">upload/อาจารย์</a>]
+        [<a href= "?controller=uploadAj&action=index">upload/อาจารย์</a>]-->
         <?php require_once("routes.php");?>
     
 </body>
