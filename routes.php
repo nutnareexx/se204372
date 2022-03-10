d806f033b1526c4373472b1f18e579d49c53f66c
KanyanutM <91516461+KanyanutM@users.noreply.github.com>
2022-03-10 17:40:41 +0700

    Update routes.php

--- routes.php
+++ routes.php
@@ -4,13 +4,10 @@ $controllers = array('pages'=>['home','error'],
                      'hamburgerAj' => ['index'],
                      'company'=>['index','newCompany'],
                      'cooperative'=>['index','newCooperative','addCooperative','search'],
-<<<<<<< Updated upstream
                      'petitionDC'=>['index','updateform','update', 'deleteconfirm', 'delete','search'],
                      'petitionC'=>['indexC','updateformC','updateC','deleteconfirmC','deleteC','searchC'],
                      'petitionDCUser'=>['index', 'newpetitionDc', 'addpetitionDc']
-=======
-                     'petitionDC'=>['index','newpetitionDc']
->>>>>>> Stashed changes
+                     
                     );
 
 function call($controller,$action){
@@ -33,7 +30,6 @@ function call($controller,$action){
         case "petitionDC":  require_once("models/petitionDcMoodel.php");
                             $controller = new petitionDcController();
                             break;
-<<<<<<< Updated upstream
 
         case "petitionC":  require_once("models/petitionCModel.php");
                             require_once("models/approveModel.php");
@@ -55,9 +51,7 @@ function call($controller,$action){
                             $controller = new petitionDcUserController();
                             break;                    
         
-=======
                             
->>>>>>> Stashed changes
         case "cooperative": require_once("models/cooperativeModel.php");
                             $controller = new cooperativeController();
                             break;                
