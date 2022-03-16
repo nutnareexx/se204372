accd8f68b44de2735ba740256b91b639852e3400
Potjarinn <potjarin.p@ku.th>
2022-03-17 00:16:01 +0700

    Create newcooperativeP.php

---
+++ views/cooperativeP/newcooperativeP.php
@@ -0,0 +1,18 @@
+<form method="get" action="">
+    <br>
+    <label> ชื่อสถานประกอบการ/หน่วยงาน <input type = "text" name = "c_name" /> </label></br>
+<label> ตำแหน่งงานที่ต้องการรับ <input type = "text" name = "c_position" /> </label></br>
+<label> แผนกฝ่าย <input type = "text" name = "c_department" /> </label></br>
+<label> จำนวน(คน) <input type = "text" name = "c_num" /> </label></br>
+<label> ความสามารถทีหรือทักษะที่ควรมี <input type = "text" name = "c_skills" /> </label></br>
+<label> ลักษณะงานที่ต้องปฏิบัติ <input type = "text" name = "c_nature" /> </label></br>
+<label> จำนวนค่าตอบแทน <input type = "text" name = "c_pay" /> </label></br>
+<label> ที่พัก <input type = "text" name = "c_room" /> </label></br>
+<label> สวัสดิการอื่นๆ <input type = "text" name = "c_benefit" /> </label></br>
+<label> การคัดเลือก <input type = "text" name = "c_select" /> </label></br>
+
+<input type="hidden" name="controller" value="cooperativeP"/>
+<button type="submit" name="action" value="index">Back</button>
+<button type="submit" name="action" value="addCooperative">Save</button>
+    
+</form>
