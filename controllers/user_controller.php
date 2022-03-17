9dd3f8d99430a083b609cbbd7b17dc682c34a81e
nutnareexx <nutnaree.co@ku.th>
2022-03-18 01:34:36 +0700

    Update user_controller.php

--- controllers/user_controller.php
+++ controllers/user_controller.php
@@ -5,9 +5,36 @@ class userController{
         $userid = $_GET['userid'];
         $password = $_GET['password'];
 
-        $uList = userModel::getAll();
+        $userList = userModel::getAll();
+        
 
-        echo $userid.$password;
+        echo "<br>username : ".$userid."<br>password :".$password."<br>";
+
+        $c=0;
+        foreach($userList as $u){
+            if($u->user_id == $userid){
+                echo "check userid";
+                $c=1;
+                break;
+            }
+        }
+
+        if($c==1){
+            $checkList = userModel::get($userid);
+            if($checkList->user_password == $password){
+                echo "<br> submit success <br>";
+            }
+            else{
+                echo "<br> password worng <br>";
+            }
+        }
+        else{
+            echo "<br> Don't have user <br>";
+        }
+
+
+
+
 
     }
 }
