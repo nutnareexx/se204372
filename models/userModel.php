35d772c3dc00915addbe98fe62cdfcf4b064ffb1
nutnareexx <nutnaree.co@ku.th>
2022-03-18 18:47:06 +0700

    Update userModel.php

--- models/userModel.php
+++ models/userModel.php
@@ -7,8 +7,9 @@ class userModel{
     public $user_phone;
     public $user_email;
     public $user_password;
+    public $user_title;
 
-    public function __construct($user_id, $name_id, $user_name, $user_surname, $user_phone, $user_email,$pass)
+    public function __construct($user_id, $name_id, $user_name, $user_surname, $user_phone, $user_email,$pass,$title)
     {
 
         $this->user_id = $user_id;
@@ -18,6 +19,7 @@ class userModel{
         $this->user_phone = $user_phone;
         $this->user_email = $user_email;
         $this->user_password = $pass;
+        $this->user_title = $title;
 
     }
 
@@ -25,7 +27,7 @@ class userModel{
     {
         $userList = [];
         require("connection_connect.php");
-        $sql = "SELECT * FROM `user`";
+        $sql = "SELECT * FROM `user` NATURAL JOIN name_title";
         $result = $conn->query($sql);
         while($my_row = $result->fetch_assoc())
         {
@@ -36,7 +38,8 @@ class userModel{
             $user_phone = $my_row['user_phone'];
             $user_email = $my_row['user_email'];
             $user_password = $my_row['user_password'];
-            $userList[] = new userModel($user_id, $name_id, $user_name, $user_surname, $user_phone, $user_email,$user_password);
+            $user_title = $my_row['name_title'];
+            $userList[] = new userModel($user_id, $name_id, $user_name, $user_surname, $user_phone, $user_email,$user_password,$user_title);
         }
         require("connection_close.php");
         return $userList;
@@ -46,7 +49,7 @@ class userModel{
     public static function get($user_id)
     {
         require("connection_connect.php");
-        $sql = "SELECT * FROM `user` WHERE user_id = '$user_id'";
+        $sql = "SELECT * FROM `user` NATURAL JOIN name_title WHERE user_id = '$user_id' ";
         $result = $conn->query($sql);
         $my_row = $result->fetch_assoc();
             $user_id = $my_row['user_id'];
@@ -56,9 +59,10 @@ class userModel{
             $user_phone = $my_row['user_phone'];
             $user_email = $my_row['user_email'];
             $user_password = $my_row['user_password'];
+            $user_title = $my_row['name_title'];
         require("connection_connect.php");
 
-        return new userModel($user_id, $name_id, $user_name, $user_surname, $user_phone, $user_email,$user_password);
+        return new userModel($user_id, $name_id, $user_name, $user_surname, $user_phone, $user_email,$user_password,$user_title);
 
     }
 
