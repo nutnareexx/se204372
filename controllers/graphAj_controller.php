f97109ad21838158fdc379258fc44565107c4742
wtmllll <91516571+wtmllll@users.noreply.github.com>
2022-03-19 21:29:46 +0700

    Update graphAj_controller.php

--- controllers/graphAj_controller.php
+++ controllers/graphAj_controller.php
@@ -4,6 +4,7 @@ class graphAjController
     public function index()
     {
         $userid = $_GET['userid'];
+        $userList=userModel::get($userid);
         require_once('views/graphAj/index_graphAj.php');
     }
 }
