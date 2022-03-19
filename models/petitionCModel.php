08519178e0236e8956e81fcf5b432ef43497d401
Potjarinn <potjarin.p@ku.th>
2022-03-19 22:24:34 +0700

    Update petitionCModel.php

--- models/petitionCModel.php
+++ models/petitionCModel.php
@@ -28,12 +28,18 @@ class petitionCModel{
     public $start_p;
     public $finish_p; 
     public $status_id;
+
+    public $name_title;
+    public $user_name;
+    public $user_surname;
+    public $status_name;
+    public $c_name;
     
  
 
     public function __construct($petition_id, $date_p,$user_id, $FB_p, $phone_p, $position_p, $approverName_p, $approverSname_p, $approverP_p,
     $dc_id, $c_id, $comName_p, $compNo_p, $compRoad_p, $compSubdist_p, $compDistrict_p, $compProvince_p, $compPost_p, $hrName_p, $hrSname_p,
-    $hrPhone_p, $hrMail_p, $salary_p, $room_p, $type_p, $start_p, $finish_p, $status_id)
+    $hrPhone_p, $hrMail_p, $salary_p, $room_p, $type_p, $start_p, $finish_p, $status_id, $name_title, $user_name, $user_surname, $status_name, $c_name)
     {
         $this->petition_id = $petition_id;
         $this->date_p =  $date_p;
@@ -42,17 +48,35 @@ class petitionCModel{
         $this->phone_p = $phone_p;
         $this->position_p = $position_p;
         $this->approverName_p = $approverName_p;
-        $this->start_p = $start_p;
-        $this->finish_p = $finish_p;
-        
-        
-        $this->c_name = $c_name;
-        $this->c_pay = $c_pay;
-        $this->c_room = $c_room;   
-        $this->c_id = $c_id; 
-        $this->status_id = $status_id;
-        $this->status_name = $status_name;
+        $this->approverSname_p = $approverSname_p;
+        $this->approverP_p = $approverP_p;
         $this->dc_id = $dc_id;
+        $this->c_id = $c_id;
+        $this->comName_p = $comName_p; 
+        $this->compNo_p = $compNo_p; 
+        $this->compRoad_p = $compRoad_p;
+        $this->compSubdist_p = $compSubdist_p;
+        $this->compDistrict_p = $compDistrict_p;
+        $this->compProvince_p = $compProvince_p;
+        $this->compPost_p = $compPost_p;
+        $this->hrName_p = $hrName_p;
+        $this->hrSname_p = $hrSname_p;
+        $this->hrPhone_p = $hrPhone_p;
+        $this->hrMail_p = $hrMail_p;
+        $this->salary_p = $salary_p;
+        $this->room_p = $room_p;
+        $this->type_p = $type_p;
+        $this->start_p = $start_p;
+        $this->finish_p = $finish_p; 
+        $this->status_id = $status_id;
+
+        $this->name_title = $name_title;
+        $this->user_name = $user_name;
+        $this->user_surname = $user_surname;
+        $this->status_name = $status_name;
+        $this->c_name = $c_name;
+       
+        
         
     }
 
@@ -60,42 +84,53 @@ class petitionCModel{
     {
         
         require("connection_connect.php");
-        $sql = "SELECT t.petition_id, t.user_id, t.name_title, t.user_name, t.user_surname, t.start_p, t.finish_p, t.date_p, t.c_position, t.c_name, t.c_pay, t.c_room, t.c_id, t.status_id, status.status_name,t.dc_id
-        FROM status INNER JOIN 
-        (SELECT uc.petition_id, uc.user_id, name_title.name_title, uc.user_name, uc.user_surname, uc.start_p, uc.finish_p, uc.date_p, uc.c_position, uc.c_name,uc.c_pay,
-            uc.c_room, uc.c_id, uc.status_id, uc.dc_id
-            FROM name_title INNER JOIN
-                (SELECT pc.petition_id, pc.user_id,user.name_id, user.user_name, user.user_surname, pc.start_p, pc.finish_p,pc.date_p, pc.c_position, pc.c_name, pc.c_pay, pc.c_room,
-                pc.c_id, pc.status_id, pc.dc_id
-                    FROM user INNER JOIN 
-                    (SELECT petition_id, user_id, start_p, finish_p,date_p, detail_cooperative.c_position, detail_cooperative.c_name, detail_cooperative.c_pay, detail_cooperative.c_room, petition.c_id,status_id, petition.dc_id
-                    FROM petition INNER JOIN detail_cooperative ON petition.c_id = detail_cooperative.c_id) AS pc ON pc.user_id = user.user_id) AS uc ON uc.name_id = name_title.name_id) AS t
-                    ON t.status_id = status.status_id
-                    WHERE petition_id = '$petition_id'
-                    ORDER BY petition_id";
+        $sql = "SELECT * FROM `petition` NATURAL JOIN `user` NATURAL JOIN `name_title` NATURAL JOIN `status`
+        NATURAL JOIN `detail_cooperative`
+         WHERE petition_id = '$petition_id'";
         $result = $conn->query($sql);
         $my_row = $result->fetch_assoc();
             
             $petition_id = $my_row['petition_id'];
+            $date_p = $my_row['date_p'];
             $user_id = $my_row['user_id'];
+            $FB_p = $my_row['FB_p'];
+            $phone_p = $my_row['phone_p'];
+            $position_p = $my_row['position_p'];
+            $approverName_p = $my_row['approverName_p'];
+            $approverSname_p = $my_row['approverSname_p'];
+            $approverP_p = $my_row['approverP_p'];
+            $dc_id = $my_row['dc_id'];
+            $c_id = $my_row['c_id'];
+            $comName_p = $my_row['comName_p']; 
+            $compNo_p = $my_row['compNo_p']; 
+            $compRoad_p = $my_row['compRoad_p']; 
+            $compSubdist_p = $my_row['compSubdist_p']; 
+            $compDistrict_p = $my_row['compDistrict_p'];
+            $compProvince_p = $my_row['compProvince_p']; 
+            $compPost_p = $my_row['compPost_p']; 
+            $hrName_p = $my_row['hrName_p']; 
+            $hrSname_p = $my_row['hrSname_p']; 
+            $hrPhone_p = $my_row['hrPhone_p']; 
+            $hrMail_p = $my_row['hrMail_p']; 
+            $salary_p = $my_row['salary_p']; 
+            $room_p = $my_row['room_p'];
+            $type_p = $my_row['type_p']; 
+            $start_p = $my_row['start_p'];
+            $finish_p = $my_row['finish_p'];    
+            $status_id = $my_row['status_id'];
+
             $name_title = $my_row['name_title'];
             $user_name = $my_row['user_name'];
             $user_surname = $my_row['user_surname'];
-            $start_p = $my_row['start_p'];
-            $finish_p = $my_row['finish_p'];
-            $date_p = $my_row['date_p'];
-            $c_position = $my_row['c_position'];
+            $status_name = $my_row['status_name'];
             $c_name = $my_row['c_name'];
-            $c_pay = $my_row['c_pay'];
-            $c_room = $my_row['c_room'];
-            $c_id = $my_row['c_id'];
-            $status_id = $my_row['status_id'];
-            $status_name = $my_row['status_name']; 
-            $dc_id = $my_row['dc_id'];
+            
+            
         require("connection_close.php");
 
-        return new petitionCModel($petition_id, $user_id, $name_title, $user_name, $user_surname, $start_p, $finish_p,
-        $date_p, $c_position, $c_name, $c_pay, $c_room, $c_id, $status_id, $status_name, $dc_id);
+        return new petitionCModel($petition_id, $date_p,$user_id, $FB_p, $phone_p, $position_p, $approverName_p, $approverSname_p, $approverP_p,
+        $dc_id, $c_id, $comName_p, $compNo_p, $compRoad_p, $compSubdist_p, $compDistrict_p, $compProvince_p, $compPost_p, $hrName_p, $hrSname_p,
+        $hrPhone_p, $hrMail_p, $salary_p, $room_p, $type_p, $start_p, $finish_p, $status_id, $name_title, $user_name, $user_surname, $status_name, $c_name);
     }
 
 
@@ -103,39 +138,49 @@ class petitionCModel{
     {
         $petionCList = [];
         require("connection_connect.php");
-        $sql = "SELECT t.petition_id, t.user_id, t.name_title, t.user_name, t.user_surname, t.start_p, t.finish_p, t.date_p, t.c_position, t.c_name, t.c_pay, t.c_room, t.c_id, t.status_id, status.status_name,t.dc_id
-        FROM status INNER JOIN 
-        (SELECT uc.petition_id, uc.user_id, name_title.name_title, uc.user_name, uc.user_surname, uc.start_p, uc.finish_p, uc.date_p, uc.c_position, uc.c_name,uc.c_pay,
-            uc.c_room, uc.c_id, uc.status_id, uc.dc_id
-            FROM name_title INNER JOIN
-                (SELECT pc.petition_id, pc.user_id,user.name_id, user.user_name, user.user_surname, pc.start_p, pc.finish_p,pc.date_p, pc.c_position, pc.c_name, pc.c_pay, pc.c_room,
-                pc.c_id, pc.status_id, pc.dc_id
-                FROM user INNER JOIN 
-                    (SELECT petition_id, user_id, start_p, finish_p,date_p, detail_cooperative.c_position, detail_cooperative.c_name, detail_cooperative.c_pay, detail_cooperative.c_room, petition.c_id,status_id, petition.dc_id
-                    FROM petition INNER JOIN detail_cooperative ON petition.c_id = detail_cooperative.c_id) AS pc ON pc.user_id = user.user_id) AS uc ON uc.name_id = name_title.name_id) AS t
-                    ON t.status_id = status.status_id
-                    GROUP BY petition_id;";
+        $sql = "SELECT * FROM `petition` NATURAL JOIN `user` NATURAL JOIN `name_title` NATURAL JOIN `status`
+        NATURAL JOIN `detail_cooperative`";
         $result = $conn->query($sql);
         while($my_row = $result->fetch_assoc())
         {
             $petition_id = $my_row['petition_id'];
+            $date_p = $my_row['date_p'];
             $user_id = $my_row['user_id'];
+            $FB_p = $my_row['FB_p'];
+            $phone_p = $my_row['phone_p'];
+            $position_p = $my_row['position_p'];
+            $approverName_p = $my_row['approverName_p'];
+            $approverSname_p = $my_row['approverSname_p'];
+            $approverP_p = $my_row['approverP_p'];
+            $dc_id = $my_row['dc_id'];
+            $c_id = $my_row['c_id'];
+            $comName_p = $my_row['comName_p']; 
+            $compNo_p = $my_row['compNo_p']; 
+            $compRoad_p = $my_row['compRoad_p']; 
+            $compSubdist_p = $my_row['compSubdist_p']; 
+            $compDistrict_p = $my_row['compDistrict_p'];
+            $compProvince_p = $my_row['compProvince_p']; 
+            $compPost_p = $my_row['compPost_p']; 
+            $hrName_p = $my_row['hrName_p']; 
+            $hrSname_p = $my_row['hrSname_p']; 
+            $hrPhone_p = $my_row['hrPhone_p']; 
+            $hrMail_p = $my_row['hrMail_p']; 
+            $salary_p = $my_row['salary_p']; 
+            $room_p = $my_row['room_p'];
+            $type_p = $my_row['type_p']; 
+            $start_p = $my_row['start_p'];
+            $finish_p = $my_row['finish_p'];    
+            $status_id = $my_row['status_id'];
+
             $name_title = $my_row['name_title'];
             $user_name = $my_row['user_name'];
             $user_surname = $my_row['user_surname'];
-            $start_p = $my_row['start_p'];
-            $finish_p = $my_row['finish_p'];
-            $date_p = $my_row['date_p'];
-            $c_position = $my_row['c_position'];
+            $status_name = $my_row['status_name'];
             $c_name = $my_row['c_name'];
-            $c_pay = $my_row['c_pay'];
-            $c_room = $my_row['c_room'];
-            $c_id = $my_row['c_id'];
-            $status_id = $my_row['status_id'];
-            $status_name = $my_row['status_name']; 
-            $dc_id = $my_row['dc_id'];
-            $petionCList[] = new petitionCModel($petition_id, $user_id, $name_title, $user_name, $user_surname, $start_p, $finish_p,
-            $date_p, $c_position, $c_name, $c_pay, $c_room, $c_id, $status_id, $status_name, $dc_id);
+
+            $petionCList[] = new petitionCModel($petition_id, $date_p,$user_id, $FB_p, $phone_p, $position_p, $approverName_p, $approverSname_p, $approverP_p,
+            $dc_id, $c_id, $comName_p, $compNo_p, $compRoad_p, $compSubdist_p, $compDistrict_p, $compProvince_p, $compPost_p, $hrName_p, $hrSname_p,
+            $hrPhone_p, $hrMail_p, $salary_p, $room_p, $type_p, $start_p, $finish_p, $status_id, $name_title, $user_name, $user_surname, $status_name, $c_name);
 
         }
         require("connection_close.php");
@@ -175,46 +220,59 @@ class petitionCModel{
     {
         $petionCList = [];
         require("connection_connect.php");
-        $sql = "SELECT t.petition_id, t.user_id, t.name_title, t.user_name, t.user_surname, t.start_p, t.finish_p, t.date_p, t.c_position, t.c_name, t.c_pay, t.c_room, t.c_id, t.status_id, status.status_name,t.dc_id
-        FROM status INNER JOIN 
-        (SELECT uc.petition_id, uc.user_id, name_title.name_title, uc.user_name, uc.user_surname, uc.start_p, uc.finish_p, uc.date_p, uc.c_position, uc.c_name,uc.c_pay,
-            uc.c_room, uc.c_id, uc.status_id, uc.dc_id
-            FROM name_title INNER JOIN
-                (SELECT pc.petition_id, pc.user_id,user.name_id, user.user_name, user.user_surname, pc.start_p, pc.finish_p,pc.date_p, pc.c_position, pc.c_name, pc.c_pay, pc.c_room,
-                pc.c_id, pc.status_id, pc.dc_id
-                FROM user INNER JOIN 
-                    (SELECT petition_id, user_id, start_p, finish_p,date_p, detail_cooperative.c_position, detail_cooperative.c_name, detail_cooperative.c_pay, detail_cooperative.c_room, petition.c_id,status_id, petition.dc_id
-                    FROM petition INNER JOIN detail_cooperative ON petition.c_id = detail_cooperative.c_id) AS pc ON pc.user_id = user.user_id) AS uc ON uc.name_id = name_title.name_id) AS t
-                    ON t.status_id = status.status_id
+        $sql = "SELECT * FROM `petition` NATURAL JOIN `user` NATURAL JOIN `name_title` NATURAL JOIN `status` NATURAL JOIN `detail_cooperative`
                      
-                    WHERE (t.petition_id like '%$key%' or t.user_id like '%$key%' 
-                    or t.name_title like '%$key%' or t.user_name like '%$key%' or t.user_surname like '%$key%' or t.start_p like '%$key%'
-                    or t.finish_p like '%$key%' or t.date_p like '%$key%' or t.c_position like '%$key%' or t.c_name like '%$key%'
-                    or t.c_pay like '%$key%'or t.c_room like '%$key%' or t.c_id like '%$key%' or t.status_id like '%$key%' 
-                    or status.status_name like '%$key%'
-                    or t.dc_id like '%$key%')
+                    WHERE (petition_id like '%$key%' or date_p like '%$key%' 
+                    or user_id like '%$key%' or FB_p like '%$key%' or phone_p like '%$key%' or position_p like '%$key%'
+                    or approverName_p like '%$key%' or approverSname_p like '%$key%' or approverP_p like '%$key%' or dc_id like '%$key%'
+                    or c_id like '%$key%'or comName_p like '%$key%' or compNo_p like '%$key%' or compRoad_p like '%$key%' 
+                    or compSubdist_p like '%$key%' or compDistrict_p like '%$key%' or compProvince_p like '%$key%' or compPost_p like '%$key%'
+                    or hrName_p like '%$key%' or hrSname_p like '%$key%' or hrPhone_p like '%$key%' or hrMail_p like '%$key%'
+                    or salary_p like '%$key%' or room_p like '%$key%' or type_p like '%$key%' or start_p like '%$key%' or finish_p like '%$key%'
+                    or status_id like '%$key%' or name_title like '%$key%' or user_name like '%$key%' or user_surname like '%$key%'
+                    or status_name like '%$key%' or c_name like '%$key%')
                     ORDER BY petition_id";
         $result = $conn->query($sql);
         while($my_row = $result->fetch_assoc())
         {
             $petition_id = $my_row['petition_id'];
+            $date_p = $my_row['date_p'];
             $user_id = $my_row['user_id'];
+            $FB_p = $my_row['FB_p '];
+            $phone_p = $my_row['phone_p'];
+            $position_p = $my_row['position_p'];
+            $approverName_p = $my_row['approverName_p'];
+            $approverSname_p = $my_row['approverSname_p'];
+            $approverP_p = $my_row['approverP_p'];
+            $dc_id = $my_row['dc_id'];
+            $c_id = $my_row['c_id'];
+            $comName_p = $my_row['comName_p']; 
+            $compNo_p = $my_row['compNo_p']; 
+            $compRoad_p = $my_row['compRoad_p']; 
+            $compSubdist_p = $my_row['compSubdist_p']; 
+            $compDistrict_p = $my_row['compDistrict_p'];
+            $compProvince_p = $my_row['compProvince_p']; 
+            $compPost_p = $my_row['compPost_p']; 
+            $hrName_p = $my_row['hrName_p']; 
+            $hrSname_p = $my_row['hrSname_p']; 
+            $hrPhone_p = $my_row['hrPhone_p']; 
+            $hrMail_p = $my_row['hrMail_p']; 
+            $salary_p = $my_row['salary_p']; 
+            $room_p = $my_row['room_p'];
+            $type_p = $my_row['type_p']; 
+            $start_p = $my_row['start_p'];
+            $finish_p = $my_row['finish_p'];    
+            $status_id = $my_row['status_id'];
+
             $name_title = $my_row['name_title'];
             $user_name = $my_row['user_name'];
             $user_surname = $my_row['user_surname'];
-            $start_p = $my_row['start_p'];
-            $finish_p = $my_row['finish_p'];
-            $date_p = $my_row['date_p'];
-            $c_position = $my_row['c_position'];
+            $status_name = $my_row['status_name'];
             $c_name = $my_row['c_name'];
-            $c_pay = $my_row['c_pay'];
-            $c_room = $my_row['c_room'];
-            $c_id = $my_row['c_id'];
-            $status_id = $my_row['status_id'];
-            $status_name = $my_row['status_name']; 
-            $dc_id = $my_row['dc_id'];
-            $petionCList[] = new petitionCModel($petition_id, $user_id, $name_title, $user_name, $user_surname, $start_p, $finish_p,
-            $date_p, $c_position, $c_name, $c_pay, $c_room, $c_id, $status_id, $status_name, $dc_id);
+
+            $petionCList[] = new petitionCModel($petition_id, $date_p,$user_id, $FB_p, $phone_p, $position_p, $approverName_p, $approverSname_p, $approverP_p,
+            $dc_id, $c_id, $comName_p, $compNo_p, $compRoad_p, $compSubdist_p, $compDistrict_p, $compProvince_p, $compPost_p, $hrName_p, $hrSname_p,
+            $hrPhone_p, $hrMail_p, $salary_p, $room_p, $type_p, $start_p, $finish_p, $status_id, $name_title, $user_name, $user_surname, $status_name, $c_name);
         }
         require("connection_close.php");
         return $petionCList;
