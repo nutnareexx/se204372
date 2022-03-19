0a10b7ea42ba22184dee288996db19abef816041
wtmllll <91516571+wtmllll@users.noreply.github.com>
2022-03-19 20:44:52 +0700

    Update checkHistory_controller.php

--- controllers/checkHistory_controller.php
+++ controllers/checkHistory_controller.php
@@ -3,7 +3,7 @@ class checkHistoryController
 {
     public function index()
     {
-        
+        $userid = $_GET['userid'];
         require_once('views/checkHistory/index_checkHistory.php');
     }
 }
