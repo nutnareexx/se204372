7b6b9a3bf3ef5f001e39b5a7bdc2bfd8718ad465
KanyanutM <91516461+KanyanutM@users.noreply.github.com>
2022-03-20 03:10:43 +0700

    Update checkApproval_controller.php

--- controllers/checkApproval_controller.php
+++ controllers/checkApproval_controller.php
@@ -3,13 +3,25 @@ class checkApprovalController
 {
     public function index()
     {
+        //$userid = $_GET['userid'];
+        //$c = $_GET['petition_id'];
+        //$petionCList = petitionCModel::get($c);
+        //$petionCList = petitionCModel::getAll();
+        //$userList=userModel::get($userid);
+        //$userList=userModel::getAll();
+        //$approveList = approveModel::getAll();
+        //$statusList = statusModel::getAll();
+        //$DetailCompanyList = DetailCompanyModel::getAll();
+        //$cooperativeList =  cooperativeModel::getAll();
+
         $userid = $_GET['userid'];
-        $c = $_GET['petition_id'];
-        $petionCList = petitionCModel::get($c);
-        $petionCList = petitionCModel::getAll();
+        
         $userList=userModel::get($userid);
-        $userList=userModel::getAll();
-        $approveList = approveModel::getAll();
+        $petionDcList = petitionDcModel::getid($userid);
+        $nametitleList = nametitleModel::getAll();
+        $userList = userModel::getAll();
+        $DetailCompanyList = DetailCompanyModel::getAll();
+        $statusList = statusModel::getAll();
         require_once('views/checkApproval/index_checkApproval.php');
     }
 }
