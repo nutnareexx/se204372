e569be823b0f5d295c082bd2fb61e64fbeab05e2
Potjarinn <potjarin.p@ku.th>
2022-03-19 22:24:25 +0700

    Update updateformC.php

--- views/petitionC/updateformC.php
+++ views/petitionC/updateformC.php
@@ -474,11 +474,8 @@ tr:nth-child(even){background-color: #f2f2f2}
             <br><b>ลำดับที่ : </b>   $petionCList->petition_id  
             <br><b>รหัสนิสิต : </b>  $petionCList->user_id
             <br><b>ชื่อ-นามสกุล : </b>  $petionCList->name_title$petionCList->user_name $petionCList->user_surname
-            <br><b>ระยะเวลาฝึกสหกิจ : </b> $petionCList->start_p - $petionCList->finish_p 
-            <br><b>ตำแหน่งที่ไปสหกิจ : </b> $petionCList->c_position
-            <br><b>ชื่อสถานประกอบการสหกิจ : </b> $petionCList->c_name    
-            <br><b>ค่าตอบแทน : </b> $petionCList->c_pay 
-            <br><b>ที่พัก : </b> $petionCList->c_room<br>";?>
+            <br><b>ชื่อสถานประกอบการสหกิจ : </b> $petionCList->c_name <br>    
+            ";?>
 
 
    
