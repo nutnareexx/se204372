d95e7f4b8afce0aa50f5a3ad4b20cecb8be45acf
KanyanutM <91516461+KanyanutM@users.noreply.github.com>
2022-03-19 21:21:08 +0700

    Update nisit_controller.php

--- controllers/nisit_controller.php
+++ controllers/nisit_controller.php
@@ -3,12 +3,8 @@ class nisitController{
     
     public function index()
     {
-<<<<<<< Updated upstream
         $userid = $_GET['userid'];
-=======
-        $userid=$_GET['userid'];
         $userList=userModel::get($userid);
->>>>>>> Stashed changes
         require_once('views/hamburger/index_hamburger.php');
     }
 }
