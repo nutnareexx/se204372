7bd15680f8ad32da6e295593ce513816b5d2108c
wtmllll <91516571+wtmllll@users.noreply.github.com>
2022-03-19 21:29:37 +0700

    Update considerAj_controller.php

--- controllers/considerAj_controller.php
+++ controllers/considerAj_controller.php
@@ -4,6 +4,7 @@ class considerAjController
     public function index()
     {
         $userid = $_GET['userid'];
+        $userList=userModel::get($userid);
         require_once('views/considerAj/index_considerAj.php');
     }
 }
