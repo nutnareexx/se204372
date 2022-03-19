c435c7aaea21bbac40b9cca52e7b0c44d31d66fd
nutnareexx <nutnaree.co@ku.th>
2022-03-19 20:19:20 +0700

    Update petitionDcUser_controller.php

--- controllers/petitionDcUser_controller.php
+++ controllers/petitionDcUser_controller.php
@@ -13,7 +13,7 @@ class petitionDcUserController
 
     public function index2()
     {
-        // $date = $_GET['date'];
+        $date = $_GET['date'];
         $userid = $_GET['userid'];
         // $phone = $_GET['phone'];
         // $faceB = $_GET['facebook'];
@@ -26,37 +26,64 @@ class petitionDcUserController
         require_once('views/petitionDcUser/newCompany_Dcuser.php');
     }
 
-    public function newpetitionDc()
-    {
-        $petionDcList = petitionDcModel::getAll();
-        //$approveList = approveModel::getAll();
-        $statusList = statusModel::getAll();
-        $nametitleList = nametitleModel::getAll();
-        //$userList = userModel::getAll();
-        $DetailCompanyList = DetailCompanyModel::getAll();
-        //$cooperativeList =  cooperativeModel::getAll();
-        require_once('views/petitionDcUser/newpetitionDc.php');
-    }
-
+    
     public function addpetitionDc()
     {
-        echo "addp";
-        $dc_id = $_GET['dc_id'];
-        $petition_id = $_GET['petition_id'];
-        $user_id = $_GET['user_id'];
-        $name_title = $_GET['name_title'];
-        $user_name = $_GET['user_name'];
-        $user_surname = $_GET['user_surname'];
-        $start_p = $_GET['start_p'];
-        $finish_p = $_GET['finish_p'];
-        $dc_position = $_GET['dc_position'];
-        $dc_name = $_GET['dc_name'];
-        $dc_pay = $_GET['dc_pay'];
-        $dc_room = $_GET['dc_room'];
-        petitionDcModel::Add($dc_id, $petition_id, $user_id, $name_title, $user_name, $user_surname, $start_p, $finish_p, 
-        $dc_position,$dc_name, $dc_pay, $dc_room);
-        petitionDcUserController::index();
+        echo "<br>addp<br>";
+        $date = $_GET['p_date'];
+        $userid = $_GET['userid'];
+        $phone = $_GET['phone'];
+        $faceB = $_GET['facebook'];
+        $position = $_GET['dc_position'];
+
+        $approve_name = $_GET['name_com'];
+        $approve_lastname = $_GET['lastname_com'];
+        $approve_position = $_GET['position'];
+
+
+        $company_no = $_GET['no'];
+        $company_road = $_GET['road'];
+        $company_sub = $_GET['Subdistrict'];
+        $company_dis = $_GET['district'];
+        $company_prov = $_GET['province'];
+        $company_code = $_GET['postcode'];
+
+        $hr_name = $_GET['namehr'];
+        $hr_lastname = $_GET['lastnamehr'];
+        $hr_phone = $_GET['phonehr'];
+        $hr_mail = $_GET['mailhr'];
+
+        $start = $_GET['start_p'];
+        $finish = $_GET['finish_p'];
+        $pay = $_GET['pay'];
+        $room = $_GET['room'];
+
+
+        if(isset($_GET['company_id'])){
+            $company_id = $_GET['company_id'];
+            echo $company_id;
+            petitionDcModel::addDC_old($date,$userid,$faceB,$phone,$position,$approve_name,$approve_lastname,$approve_position,$company_id,$company_no,$company_road,$company_sub,$company_dis,$company_prov,$company_code,$hr_name,$hr_lastname,$hr_phone,$hr_mail,$pay,$room,$start,$finish);
+        }
+        if(isset($_GET['dc_name'])){
+            $company_name = $_GET['dc_name'];
+            echo $company_name;
+            petitionDcModel::addDC_new($date,$userid,$faceB,$phone,$position,$approve_name,$approve_lastname,$approve_position,$company_name,$company_no,$company_road,$company_sub,$company_dis,$company_prov,$company_code,$hr_name,$hr_lastname,$hr_phone,$hr_mail,$pay,$room,$start,$finish);
+        }
+        
+        
+
     }
+    public function newpetitionDc()
+        {
+            $petionDcList = petitionDcModel::getAll();
+            //$approveList = approveModel::getAll();
+            $statusList = statusModel::getAll();
+            $nametitleList = nametitleModel::getAll();
+            //$userList = userModel::getAll();
+            $DetailCompanyList = DetailCompanyModel::getAll();
+            //$cooperativeList =  cooperativeModel::getAll();
+            require_once('views/petitionDcUser/newpetitionDc.php');
+        }
 
     public function updateform()
     {
