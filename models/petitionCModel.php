75ebc9ee6653f2db9ce86f46c3ba7571a2f4055f
Potjarinn <potjarin.p@ku.th>
2022-03-19 20:44:23 +0700

    Update petitionCModel.php

--- models/petitionCModel.php
+++ models/petitionCModel.php
@@ -1,35 +1,51 @@
 <?php
 class petitionCModel{
     public $petition_id;
-    public $user_id;
-    public $name_title;
-    public $user_name;
-    public $user_surname;
-    public $start_p;
-    public $finish_p;
     public $date_p;
-    public $c_position;
-    public $c_name;
-    public $c_pay;
-    public $c_room;   
-    public $c_id;
-    public $status_id;
-    public $status_name;
+    public $user_id;
+    public $FB_p;
+    public $phone_p;
+    public $position_p;
+    public $approverName_p;
+    public $approverSname_p;
+    public $approverP_p;
     public $dc_id;
+    public $c_id;
+    public $comName_p;
+    public $compNo_p;
+    public $compRoad_p;
+    public $compSubdist_p;
+    public $compDistrict_p;
+    public $compProvince_p;
+    public $compPost_p;
+    public $hrName_p;
+    public $hrSname_p;
+    public $hrPhone_p;
+    public $hrMail_p;
+    public $salary_p;
+    public $room_p;
+    public $type_p;
+    public $start_p;
+    public $finish_p; 
+    public $status_id;
+    
  
 
-    public function __construct($petition_id, $user_id, $name_title, $user_name, $user_surname, $start_p, $finish_p,
-    $date_p, $c_position, $c_name, $c_pay, $c_room, $c_id, $status_id, $status_name, $dc_id)
+    public function __construct($petition_id, $date_p,$user_id, $FB_p, $phone_p, $position_p, $approverName_p, $approverSname_p, $approverP_p,
+    $dc_id, $c_id, $comName_p, $compNo_p, $compRoad_p, $compSubdist_p, $compDistrict_p, $compProvince_p, $compPost_p, $hrName_p, $hrSname_p,
+    $hrPhone_p, $hrMail_p, $salary_p, $room_p, $type_p, $start_p, $finish_p, $status_id)
     {
         $this->petition_id = $petition_id;
+        $this->date_p =  $date_p;
         $this->user_id = $user_id;
-        $this->name_title = $name_title;
-        $this->user_name = $user_name;
-        $this->user_surname = $user_surname;
+        $this->FB_p = $FB_p;
+        $this->phone_p = $phone_p;
+        $this->position_p = $position_p;
+        $this->approverName_p = $approverName_p;
         $this->start_p = $start_p;
         $this->finish_p = $finish_p;
-        $this->date_p =  $date_p;
-        $this->c_position = $c_position;
+        
+        
         $this->c_name = $c_name;
         $this->c_pay = $c_pay;
         $this->c_room = $c_room;   
