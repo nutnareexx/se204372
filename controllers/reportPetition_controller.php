9e9e2bf705c7518f1d7d7a8d0a869861638fb968
wtmllll <91516571+wtmllll@users.noreply.github.com>
2022-03-19 21:29:58 +0700

    Update reportPetition_controller.php

--- controllers/reportPetition_controller.php
+++ controllers/reportPetition_controller.php
@@ -4,6 +4,7 @@ class reportPetitionController
     public function index()
     {
         $userid = $_GET['userid'];
+        $userList=userModel::get($userid);
         require_once('views/reportPetition/index_reportPetition.php');
     }
 }
