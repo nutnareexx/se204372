64aa3cf3c577a9d90bb78628fe4183643c7fffc4
KanyanutM <91516461+KanyanutM@users.noreply.github.com>
2022-03-20 00:50:15 +0700

    Update checkApproval_controller.php

--- controllers/checkApproval_controller.php
+++ controllers/checkApproval_controller.php
@@ -4,7 +4,12 @@ class checkApprovalController
     public function index()
     {
         $userid = $_GET['userid'];
+        $c = $_GET['petition_id'];
+        $petionCList = petitionCModel::get($c);
+        $petionCList = petitionCModel::getAll();
         $userList=userModel::get($userid);
+        $userList=userModel::getAll();
+        $approveList = approveModel::getAll();
         require_once('views/checkApproval/index_checkApproval.php');
     }
 }
