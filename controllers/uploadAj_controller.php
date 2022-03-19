b22adf841d1635e221dcefc7f665d7b167b7858e
wtmllll <91516571+wtmllll@users.noreply.github.com>
2022-03-19 21:30:00 +0700

    Update uploadAj_controller.php

--- controllers/uploadAj_controller.php
+++ controllers/uploadAj_controller.php
@@ -4,6 +4,7 @@ class uploadAjController
     public function index()
     {
         $userid = $_GET['userid'];
+        $userList=userModel::get($userid);
         require_once('views/uploadAj/index_uploadAj.php');
     }
 }
