2f7c4942e4d5b61a70f7ab554a9b827295bb2b00
Potjarinn <potjarin.p@ku.th>
2022-03-20 21:51:41 +0700

    Update petitionC_controller.php

--- controllers/petitionC_controller.php
+++ controllers/petitionC_controller.php
@@ -29,39 +29,6 @@ class petitionCController
     }
 
 
-    public function newpetitionC()
-    {
-        $userid = $_GET['userid'];
-        $petionCList = petitionCModel::getAll();
-        //$approveList = approveModel::getAll();
-        $statusList = statusModel::getAll();
-        $nametitleList = nametitleModel::getAll();
-        //$userList = userModel::getAll();
-        //$DetailCompanyList = DetailCompanyModel::getAll();
-        $cooperativeList =  cooperativeModel::getAll();
-        require_once('views/petitionDc/newpetitionDc.php');
-    }
-
-    public function addpetitionC()
-    {
-       
-        //$userid = $_GET['userid'];
-        $dc_id = $_GET['dc_id'];
-        $petition_id = $_GET['petition_id'];
-        $user_id = $_GET['user_id'];
-        $name_title = $_GET['name_title'];
-        $user_name = $_GET['user_name'];
-        $user_surname = $_GET['user_surname'];
-        $start_p = $_GET['start_p'];
-        $finish_p = $_GET['finish_p'];
-        $dc_position = $_GET['dc_position'];
-        $dc_name = $_GET['dc_name'];
-        $dc_pay = $_GET['dc_pay'];
-        $dc_room = $_GET['dc_room'];
-        petitionCModel::Add($dc_id, $petition_id, $user_id, $name_title, $user_name, $user_surname, $start_p, $finish_p, 
-        $dc_position,$dc_name, $dc_pay, $dc_room);
-        petitionCController::indexC();
-    }
 
     public function updateformC()
     {
@@ -81,6 +48,27 @@ class petitionCController
         $petition_id = $_GET['petitionid'];
         $status_id = $_GET['status_id'];
         petitionCModel::update( $petition_id, $status_id);
+        petitionCController::indexC();
+    }
+
+    public function updateformnewC()
+    {
+        $userid = $_GET['userid'];
+        $c = $_GET['petition_id'];
+        $petionCnewList = petitionnewCModel::getnew($c);
+        $statusList = statusModel::getAll();
+        $userList = userModel::get($userid);
+
+        require_once('views/petitionC/updateformnewC.php');
+
+    }
+
+    public function updatenewC()
+    {
+        $userid = $_GET['userid'];
+        $petition_id = $_GET['petitionid'];
+        $status_id = $_GET['status_id'];
+        petitionnewCModel::update( $petition_id, $status_id);
         petitionCController::indexC();
     }
 
