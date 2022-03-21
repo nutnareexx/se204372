f331d47ff10652add33b9375c8015781c42641c0
KanyanutM <91516461+KanyanutM@users.noreply.github.com>
2022-03-22 01:36:33 +0700

    Update Aj_controller.php

--- controllers/Aj_controller.php
+++ controllers/Aj_controller.php
@@ -5,6 +5,7 @@ class AjController
     {
         $userid = $_GET['userid'];
         $userList=userModel::get($userid);
+        $fList = fileModel::getAll();
         require_once('views/hamburger/index_hamburgerAj.php');
     }
 }
