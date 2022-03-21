76f02e1ce06adc924b06a42d934440b53254e723
KanyanutM <91516461+KanyanutM@users.noreply.github.com>
2022-03-22 01:37:00 +0700

    Update pages_controller.php

--- controllers/pages_controller.php
+++ controllers/pages_controller.php
@@ -3,6 +3,7 @@ class PagesController
 {
     public function home()
     {
+        $fList = fileModel::getAll();
         require_once('views/hamburger/index_home.php');
     }
     public function error()
