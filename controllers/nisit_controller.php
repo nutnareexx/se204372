87f32a5bd154a50bb303fe39a09e7c4480afe259
KanyanutM <91516461+KanyanutM@users.noreply.github.com>
2022-03-22 01:37:04 +0700

    Update nisit_controller.php

--- controllers/nisit_controller.php
+++ controllers/nisit_controller.php
@@ -5,6 +5,7 @@ class nisitController{
     {
         $userid = $_GET['userid'];
         $userList=userModel::get($userid);
+        $fList = fileModel::getAll();
         require_once('views/hamburger/index_hamburger.php');
     }
 
