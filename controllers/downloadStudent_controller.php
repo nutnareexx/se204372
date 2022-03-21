1131083ed582ddee3db5c83d1541a0f25482e85b
KanyanutM <91516461+KanyanutM@users.noreply.github.com>
2022-03-21 23:51:30 +0700

    Update downloadStudent_controller.php

--- controllers/downloadStudent_controller.php
+++ controllers/downloadStudent_controller.php
@@ -4,7 +4,9 @@ class downloadStudentController
     public function index()
     {
         $userid = $_GET['userid'];
-        $userList=userModel::get($userid);
+        if($userid != "x"){
+            $userList=userModel::get($userid);
+        }
         require_once('views/downloadStudent/index_downloadStudent.php');
     }
 }
