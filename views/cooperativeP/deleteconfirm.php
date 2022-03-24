308d96b68f0122527ed23386b93ed5af7a723012
KanyanutM <91516461+KanyanutM@users.noreply.github.com>
2022-03-25 01:29:32 +0700

    Update deleteconfirm.php

--- views/cooperativeP/deleteconfirm.php
+++ views/cooperativeP/deleteconfirm.php
@@ -4,7 +4,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-        
+        <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
         <link href='https://unpkg.com/boxicons@2.1.1/css/boxicons.min.css' rel='stylesheet'>
         <link href="https://fonts.googleapis.com/css2?family=Prompt&display=swap" rel="stylesheet">
 </head>
@@ -325,6 +325,7 @@ body {
                 font-family: 'Prompt', sans-serif;
                 background-color: #f1f1f1;
                 text-align: center;
+               
             }
 
 .table {
@@ -407,6 +408,11 @@ body {
                 box-sizing: border-box;
             
             }
+            .head2{
+            font-family: 'Prompt', sans-serif;
+            font-size: 32px;
+            font-weight: bold;
+        }
 
 
 </style>
@@ -416,7 +422,7 @@ body {
 
 <div class="sidebar close">
         <div class="logo-details">
-            <i class='bx bxs-tree-alt'></i>
+            <i class='bx bx-buildings'></i>
             <span class="logo_name">KASETSART UNIVERSITY</span>
         </div>
     <dl class="nav-links">
@@ -457,52 +463,6 @@ body {
                 <li><a href="?controller=petitionC&action=indexC&userid=<?php echo $userid;?>">สหกิจ</a></li>
             </ul>
         </li>
-        
-        <li>
-            <a href="?controller=considerAj&action=index&userid=<?php echo $userid;?>">
-                <i class='bx bx-file'></i>
-                <span class="link_name">พิจาราณาคำร้องขอฝึกงาน</span>
-            </a>
-            <ul class="sub-menu blank">
-                <li><a class="link_name" href="?controller=considerAj&action=index&userid=<?php echo $userid;?>">พิจาราณาคำร้องขอฝึกงาน</a></li>
-            </ul>
-        </li>
-        <li>
-            <a href="?controller=graphAj&action=index&userid=<?php echo $userid;?>">
-                <i class='bx bx-line-chart' ></i>
-                <span class="link_name">สถานการณ์ยื่นคำร้องนิสิต</span>
-            </a>
-            <ul class="sub-menu blank">
-                <li><a class="link_name" href="?controller=graphAj&action=index&userid=<?php echo $userid;?>">สถานการณ์ยื่นคำร้องนิสิต</a></li>
-            </ul>
-        </li>
-        <li>
-            <a href="?controller=reportPetition&action=index&userid=<?php echo $userid;?>">
-                <i class='bx bx-file-blank' ></i>
-                <span class="link_name">รายงานสรุปการอนุมัติฝึกงาน</span>
-            </a>
-            <ul class="sub-menu blank">
-                <li><a class="link_name" href="?controller=reportPetition&action=index&userid=<?php echo $userid;?>">รายงานสรุปการอนุมัติฝึกงาน</a></li>
-            </ul>
-        </li>
-        <li>
-            <a href="?controller=bookAj&action=index&userid=<?php echo $userid;?>">
-                <i class='bx bx-book' ></i>
-                <span class="link_name">ออกหนังสือขอความอนุเคราะห์</span>
-            </a>
-            <ul class="sub-menu blank">
-                <li><a class="link_name" href="?controller=bookAj&action=index&userid=<?php echo $userid;?>">ออกหนังสือขอความอนุเคราะห์</a></li>
-            </ul>
-        </li>
-        <li>
-            <a href="?controller=checkHistory&action=index&userid=<?php echo $userid;?>">
-                <i class='bx bx-notepad'></i>
-                <span class="link_name">ตรวจสอบประวัติการฝึกงาน</span>
-            </a>
-            <ul class="sub-menu blank">
-                <li><a class="link_name" href="?controller=checkHistory&action=index&userid=<?php echo $userid;?>">ตรวจสอบประวัติการฝึกงาน</a></li>
-            </ul>
-        </li>
         <li>
             <a href="?controller=uploadAj&action=index&userid=<?php echo $userid;?>">
                 <i class='bx bx-upload'></i>
@@ -538,8 +498,8 @@ body {
     </div>
     <!-- เขียนตรงนี้นาจาาาาา -->
     <div class="header">
-        
-<h2>ยืนยันที่จะลบข้อมูลนี้หรือไม่? <h2>
+    <div class="head2"> ยืนยันที่จะลบข้อมูลนี้หรือไม่? </div>   
+
     
 </div>
 
@@ -596,6 +556,7 @@ body {
 
 <form method="get" action="">
     <input type="hidden" name="controller" value="cooperativeP"/>
+    <input type="hidden" name="userid" value="<?php echo $userid;?>"/>
     <input type="hidden" name="cid" value="<?php echo $cooperativePList->c_id;?>"/>
     <button type="submit" name="action" value="index">Back</button>
     <button type="submit" name="action" value="delete">Delete</button>
