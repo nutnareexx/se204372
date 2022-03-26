1a9944b48a382f0cb3267e31eb2b506249386178
wtmllll <91516571+wtmllll@users.noreply.github.com>
2022-03-26 22:48:21 +0700

    Update index2_Dcuser.php

--- views/petitionDcUser/index2_Dcuser.php
+++ views/petitionDcUser/index2_Dcuser.php
@@ -1,7 +1,6 @@
 <!DOCTYPE html>
 <html>
     <head>
-            <!-- <link rel ="stylesheet" href="hamburger.css"> -->
             <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
             <link href='https://unpkg.com/boxicons@2.1.1/css/boxicons.min.css' rel='stylesheet'>
             <link href="https://fonts.googleapis.com/css2?family=Prompt&display=swap" rel="stylesheet">
@@ -30,9 +29,6 @@
             width: 78px;
         }
 
-        /*.sidebar a:hover {
-            color: #f1f1f1;
-        }*/
         .sidebar .logo-details{
             height: 60px;
             width: 100%;
@@ -65,7 +61,6 @@
         }
 
         .sidebar .nav-links{
-            /*   background: red;*/
             height: 100%;
             padding-top:30px 0 150px 0;
             overflow: auto;
@@ -298,6 +293,7 @@
             text-align: center;
             white-space: nowrap;
         }
+
         .sidebar.close .profile-details li i,
         .sidebar.close .profile-details .profile_name,
         .sidebar.close .profile-details a{
@@ -331,8 +327,6 @@
         }
 
         table {
-            /* font-family: 'Prompt', sans-serif; */
-            /* border-collapse: collapse; */
             font-size: 18px;
             font-weight: bold;
             width: 80%;
@@ -401,6 +395,7 @@
                 box-sizing: border-box;
             
             }
+
         input[type=email], select {
                 font-family: 'Prompt', sans-serif;
                 width: 100%;
@@ -524,7 +519,6 @@
 
                 <tr>
                     <td align="right" style="width: 30%;" ><label>วันที่เขียนคำร้อง :</label></td>
-                    <!-- <td> <input type="date" name="date"/></td> -->
                     <td><label>  <input type="date" name="p_date" value="<?php echo $date;?>"/></label></td>
                 </tr>
 
@@ -537,7 +531,6 @@
                 <tr>
                     <td align="right"><label>รหัสนิสิต :</label></td>
                     <td><?php echo $userList->user_id;?></td>
-                    <!-- <td> <input type="text" name="user_id"/></td> -->
                 </tr>
                 
                 <tr>
@@ -695,13 +688,6 @@
                 <label for="dh"> ไม่มี </label><br>
                 
             </tr>
-
-            <!-- <tr>
-                <form action="petitionDcUser_controller.php" method="post" enctype="multipart/form-data" name="upfile" id="upfile">
-                <td align="right"><label>ใบคำร้องขอฝึกงาน : </label></td>
-                <td><input type="button" name="file_name" value="เพิ่มไฟล์"></td>
-                
-            </tr> -->
             
             </table>
             <br>
