8552474b7b0fe553a9abe4fa59131ae77eda4dd3
wtmllll <91516571+wtmllll@users.noreply.github.com>
2022-03-26 22:48:25 +0700

    Update index3_Dcuser.php

--- views/petitionDcUser/index3_Dcuser.php
+++ views/petitionDcUser/index3_Dcuser.php
@@ -1,7 +1,6 @@
 <!DOCTYPE html>
 <html>
     <head>
-            <!-- <link rel ="stylesheet" href="hamburger.css"> -->
             <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
             <link href='https://unpkg.com/boxicons@2.1.1/css/boxicons.min.css' rel='stylesheet'>
             <link href="https://fonts.googleapis.com/css2?family=Prompt&display=swap" rel="stylesheet">
@@ -30,9 +29,6 @@
             width: 78px;
         }
 
-        /*.sidebar a:hover {
-            color: #f1f1f1;
-        }*/
         .sidebar .logo-details{
             height: 60px;
             width: 100%;
@@ -65,7 +61,6 @@
         }
 
         .sidebar .nav-links{
-            /*   background: red;*/
             height: 100%;
             padding-top:30px 0 150px 0;
             overflow: auto;
@@ -331,8 +326,6 @@
         }
 
         table {
-            /* font-family: 'Prompt', sans-serif; */
-            /* border-collapse: collapse; */
             font-size: 18px;
             font-weight: bold;
             width: 80%;
@@ -401,6 +394,7 @@
                 box-sizing: border-box;
             
             }
+
         input[type=email], select {
                 font-family: 'Prompt', sans-serif;
                 width: 100%;
@@ -412,6 +406,7 @@
                 box-sizing: border-box;
             
             }
+
         input[type=file], select {
                 font-family: 'Prompt', sans-serif;
                 width: 100%;
@@ -536,7 +531,6 @@
 
                 <tr>
                     <td align="right" style="width: 30%;" ><label>วันที่เขียนคำร้อง :</label></td>
-                    <!-- <td> <input type="date" name="date"/></td> -->
                     <td><label>  <input type="date" name="p_date" value="<?php echo $date;?>"/></label></td>
                 </tr>
 
@@ -549,7 +543,6 @@
                 <tr>
                     <td align="right"><label>รหัสนิสิต :</label></td>
                     <td><?php echo $userList->user_id;?></td>
-                    <!-- <td> <input type="text" name="user_id"/></td> -->
                 </tr>
                 
                 <tr>
@@ -601,7 +594,6 @@
                 
             </th>
 
-            <!-- ยังงไม่ได้แก้ -->
             <tr>
                 <td align="right"><label>ชื่อสถานประกอบการ : </label></td>
                 <td><?php 
@@ -683,7 +675,6 @@
                 <td align="right"><label>เริ่มฝึกงาน : </label></td>
                 <td>
                     <?php 
-                        // echo $start."<br>";
                         $year = substr($start, 0 ,4);
                         $mont = substr($start, 5 ,2);
                         $day = substr($start, 8 ,2);
@@ -696,7 +687,6 @@
                 <td align="right"><label>สิ้นสุดการฝึกงาน : </label></td>
                 <td>
                     <?php 
-                        // echo $finish;
                         $year = substr($finish, 0 ,4);
                         $mont = substr($finish, 5 ,2);
                         $day = substr($finish, 8 ,2);
@@ -764,13 +754,9 @@
 
             <?php
             if($c=="cid"){
-                // $company_id = $_GET['company_id'];
-                // echo "cid";
                 echo "<input type='hidden' name='company_id' value='$company_id'/>";
             }
             else if($c=="name"){
-                // $company_name = $_GET['dc_name'];
-                // echo $company_name;
                 echo "<input type='hidden' name='company_name' value='$company_name'/>";
             }
             ?>
@@ -791,22 +777,22 @@
 
     <script>
 
-let arrow = document.querySelectorAll(".arrow");
-for (var i = 0; i < arrow.length; i++){
-    arrow[i].addEventListener("click", (e)=>{
-        let arrowParent = e.target.parentElement.parentElement;
-        arrowParent.classList.toggle("showMenu");
-    });
-}
+        let arrow = document.querySelectorAll(".arrow");
+        for (var i = 0; i < arrow.length; i++){
+            arrow[i].addEventListener("click", (e)=>{
+                let arrowParent = e.target.parentElement.parentElement;
+                arrowParent.classList.toggle("showMenu");
+            });
+        }
 
-let sidebar = document.querySelector(".sidebar");
-let sidebarBtn = document.querySelector(".bx-menu");
-console.log(sidebarBtn);
-sidebarBtn.addEventListener("click", ()=>{
-    sidebar.classList.toggle("close");
-});
+        let sidebar = document.querySelector(".sidebar");
+        let sidebarBtn = document.querySelector(".bx-menu");
+        console.log(sidebarBtn);
+        sidebarBtn.addEventListener("click", ()=>{
+            sidebar.classList.toggle("close");
+        });
 
-</script>
+    </script>
 
 
     </body>
