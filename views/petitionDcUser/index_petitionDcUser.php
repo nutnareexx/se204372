dbf7728d6a870ad986940738975656ac9a8d97d2
wtmllll <91516571+wtmllll@users.noreply.github.com>
2022-03-26 22:49:03 +0700

    Update index_petitionDcUser.php

--- views/petitionDcUser/index_petitionDcUser.php
+++ views/petitionDcUser/index_petitionDcUser.php
@@ -1,7 +1,6 @@
 <!DOCTYPE html>
 <html>
     <head>
-            <!-- <link rel ="stylesheet" href="hamburger.css"> -->
             <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
             <link href='https://unpkg.com/boxicons@2.1.1/css/boxicons.min.css' rel='stylesheet'>
             <link href="https://fonts.googleapis.com/css2?family=Prompt&display=swap" rel="stylesheet">
@@ -30,9 +29,6 @@
             width: 78px;
         }
 
-        /*.sidebar a:hover {
-            color: #f1f1f1;
-        }*/
         .sidebar .logo-details{
             height: 60px;
             width: 100%;
@@ -65,7 +61,6 @@
         }
 
         .sidebar .nav-links{
-            /*   background: red;*/
             height: 100%;
             padding-top:30px 0 150px 0;
             overflow: auto;
@@ -121,7 +116,6 @@
         .sidebar .nav-links li a{
             display: flex;
             align-items: center;
-            /* margin: 10px 10px 10px 10px; */
             text-decoration: none;
             white-space: nowrap;
         }
@@ -333,8 +327,6 @@
         }
 
         table {
-            /* font-family: 'Prompt', sans-serif; */
-            /* border-collapse: collapse; */
             font-size: 18px;
             font-weight: bold;
             width: 80%;
@@ -531,7 +523,6 @@
                 <tr>
                     <td align="right"><label>รหัสนิสิต :</label></td>
                     <td><?php echo $userList->user_id;?></td>
-                    <!-- <td> <input type="text" name="user_id"/></td> -->
                 </tr>
                 
                 <tr>
